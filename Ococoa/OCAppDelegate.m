82af1853043c6b6bc454af7a57c0bfdf1f7e2373
Philippe Casgrain <github@casgrain.com>
2011-12-18 12:03:53 -0500

    Application is now set up to receive notifications.

--- Ococoa/OCAppDelegate.m
+++ Ococoa/OCAppDelegate.m
@@ -29,7 +29,29 @@
     }
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+
+    // Let the device know we want to receive push notifications
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+
     return YES;
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", [error description]);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    for (id key in userInfo)
+    {
+        NSLog(@"key: %@, value: %@", key, [userInfo objectForKey:key]);
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
