033b2e98c399b3cc9188021868ffd8f93645b625
Greg Robertson <trifusion@gmail.com>
2012-01-15 18:05:36 -0500

    Added Reachability to App Delegate
    Added Reachability code to App Delegate for initial connection to
    UrbanAirship

--- Ococoa/OCAppDelegate.m
+++ Ococoa/OCAppDelegate.m
@@ -11,6 +11,7 @@
 #import "OCViewController.h"
 #import "OCPrivateInfo.h"
 #import "OCDoorbell.h"
+#import "Reachability.h"
 
 @implementation OCAppDelegate
 
@@ -51,23 +52,37 @@
 {
     NSLog(@"My token is: %@", deviceToken);
 
-    // Convert the token to a hex string and make sure it's all caps
-    NSMutableString *tokenString = [NSMutableString stringWithString:[[deviceToken description] uppercaseString]];
-    [tokenString replaceOccurrencesOfString:@"<" withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
-    [tokenString replaceOccurrencesOfString:@">" withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
-    [tokenString replaceOccurrencesOfString:@" " withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
+    Reachability *r = [Reachability reachabilityWithHostName:@"https://go.urbanairship.com/"];
+    NetworkStatus internetStatus = [r currentReachabilityStatus];
+    if (internetStatus == NotReachable)
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Network Status" 
+                                                        message:@"Sorry, the network does not appear to be available. Please try again later." 
+                                                       delegate:nil 
+                                              cancelButtonTitle:nil 
+                                              otherButtonTitles:@"OK", nil];
+        [alert show];
+    }
+    else
+    {
+        // Convert the token to a hex string and make sure it's all caps
+        NSMutableString *tokenString = [NSMutableString stringWithString:[[deviceToken description] uppercaseString]];
+        [tokenString replaceOccurrencesOfString:@"<" withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
+        [tokenString replaceOccurrencesOfString:@">" withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
+        [tokenString replaceOccurrencesOfString:@" " withString:@"" options:0 range:NSMakeRange(0, tokenString.length)];
 
-    // Create the NSURL for the request
-    NSString *urlFormat = @"https://go.urbanairship.com/api/device_tokens/%@";
-    NSURL *registrationURL = [NSURL URLWithString:[NSString stringWithFormat:urlFormat, tokenString]];
+        // Create the NSURL for the request
+        NSString *urlFormat = @"https://go.urbanairship.com/api/device_tokens/%@";
+        NSURL *registrationURL = [NSURL URLWithString:[NSString stringWithFormat:urlFormat, tokenString]];
 
-    // Create the registration request
-    NSMutableURLRequest *registrationRequest = [[NSMutableURLRequest alloc] initWithURL:registrationURL];
-    [registrationRequest setHTTPMethod:@"PUT"];
+        // Create the registration request
+        NSMutableURLRequest *registrationRequest = [[NSMutableURLRequest alloc] initWithURL:registrationURL];
+        [registrationRequest setHTTPMethod:@"PUT"];
 
-    // And fire it off
-    NSURLConnection *connection = [NSURLConnection connectionWithRequest:registrationRequest delegate:self];
-    [connection start];
+        // And fire it off
+        NSURLConnection *connection = [NSURLConnection connectionWithRequest:registrationRequest delegate:self];
+        [connection start];
+    }
 }
 
 - (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
