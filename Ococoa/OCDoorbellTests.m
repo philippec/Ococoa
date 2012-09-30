3291b6a9818d8d0dd2798697203199b8daa2d4f7
Philippe Casgrain <github@casgrain.com>
2012-09-30 16:55:19 -0400

    Can read and write JSON data

--- Ococoa/OCDoorbellTests.m
+++ Ococoa/OCDoorbellTests.m
@@ -56,9 +56,30 @@
     [[mockAlert expect] textFieldAtIndex:1];
     [[mockAlert expect] show];
 
-    STAssertNoThrow([self.doorBell ring], @"");
+    STAssertNoThrow([self.doorBell ring:nil], @"");
 
     [mockAlert verify];
 }
 
+- (void)testRingAtFrontDoor
+{
+    id mockAlert = [OCMockObject mockForClass:[UIAlertView class]];
+
+    STAssertNoThrow(self.doorBell.alertView = mockAlert, @"");
+
+    CLLocation *inFrontOfCodeFactory = [[CLLocation alloc] initWithLatitude:45.420 longitude:-75.702];
+
+    // This will always return YES, so it cannot be mocked properly
+    // see http://stackoverflow.com/questions/11098615/how-to-stub-respondstoselector-method
+    //[[mockAlert expect] respondsToSelector:@selector(setAlertViewStyle:)];
+    [[mockAlert expect] setAlertViewStyle:UIAlertViewStyleLoginAndPasswordInput];
+    [[mockAlert expect] textFieldAtIndex:0];
+    [[mockAlert expect] textFieldAtIndex:0];
+    [[mockAlert expect] textFieldAtIndex:1];
+    [[mockAlert expect] show];
+
+    STAssertNoThrow([self.doorBell ring:inFrontOfCodeFactory], @"");
+
+    [mockAlert verify];
+}
 @end
